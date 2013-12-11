42eada0af96ddf3cd464d976c86901207d744c47
daleum24 <dale.um@gmail.com>
2013-12-11 10:45:23 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ImgurClone::Application.config.secret_token = '2bdea818bfabaa47e8c00c372979eb102631132013e8a0191b7b34e871b38d56e0632205b18844148ea60dd0047c5df3861f44f6a3e46d759fdb5e98a5c8a9a0'
