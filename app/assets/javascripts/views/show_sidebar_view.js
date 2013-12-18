daaa5ecc2b6f63022df8f5b4aede9b8f9f00e1fe
Dale Um <dale.um@gmail.com>
2013-12-18 15:03:43 -0500

    removes photo map view

--- app/assets/javascripts/views/show_sidebar_view.js
+++ app/assets/javascripts/views/show_sidebar_view.js
@@ -1,13 +1,10 @@
 ImgurClone.Views.ShowSideBarView = Backbone.View.extend({
 	initialize: function(){
 		this.$el.addClass("show-sidebar")
-		this.photoMapView = new ImgurClone.Views.PhotoMapView();
 	},
 	
 	render: function(callback){
-		this.$el.append(this.photoMapView.render().$el)
-		
-		// var map = L.mapbox.map('map', 'examples.map-y7l23tes').setView([37.9, -77], 5)
+		this.$el.append($("<div id='photo-map'></div>"))
 		return this
 	}
 });
