02130584780e2c350c2d0d8c436bf90c9b1d286f
Dale Um <dale.um@gmail.com>
2013-12-18 20:39:42 -0500

    stores current users location

--- app/assets/javascripts/imgur_clone.js
+++ app/assets/javascripts/imgur_clone.js
@@ -5,6 +5,17 @@ window.ImgurClone = {
   Routers: {},
   initialize: function(user_id, photos, users, regions) {
 		
+		function store_location(position){
+			var latitude  = ImgurClone.CurrLat = position.coords.latitude;
+			var longitude = ImgurClone.CurrLng = position.coords.longitude;
+		}
+		
+		function get_location() {
+		  navigator.geolocation.getCurrentPosition(store_location);
+		}
+		
+		get_location();
+		
 		var csrf_token = ImgurClone.csrf_token = $('meta[name="csrf-token"]').attr('content');
 		var user_id = ImgurClone.user_id = +user_id
 		var PhotosCollection = ImgurClone.PhotosCollection = new ImgurClone.Collections.Photos(JSON.parse(photos), {parse: true})
