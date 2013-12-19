9fc38703e572244fd874ca9154dd5e1f694bb281
Dale Um <dale.um@gmail.com>
2013-12-19 02:27:12 -0500

    updates regions page

--- app/assets/javascripts/views/regions_view.js
+++ app/assets/javascripts/views/regions_view.js
@@ -3,8 +3,19 @@ ImgurClone.Views.RegionsView = Backbone.View.extend({
 		this.$el.addClass("regions-container")
 	},
 	
+	regionsTemplate: JST['regions_header'],
+	
+	events:{
+		"change .region-select": "display_region"
+	},
+	
+	display_region: function(event){
+		event.preventDefault();
+		alert("holy fuck!!")
+	},
+	
 	render: function(){
-		this.$el.append($("<div id='region-header'></div>"))
+		this.$el.append(this.regionsTemplate({myRegions: ImgurClone.RegionsCollection}))
 		this.$el.append($("<div id='region-map' class='dark' ></div>"))
 		return this
 	}
