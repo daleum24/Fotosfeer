4713373a9f66e8a4c6cdc08a0565e0c2ebf13216
Dale Um <dale.um@gmail.com>
2014-01-05 15:58:49 -0500

    creates favorite specs

--- spec/models/favorite_spec.rb
+++ spec/models/favorite_spec.rb
@@ -1,5 +1,20 @@
 require 'spec_helper'
 
 describe Favorite do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "without user_id and photo_id" do
+    let(:incomplete_favorite) { favorite = Favorite.new }
+    
+    it "validates presence of user_id" do
+      expect(incomplete_favorite).to have(1).error_on(:user_id)
+    end
+    
+    it "validates presence of photo_id" do
+      expect(incomplete_favorite).to have(1).error_on(:photo_id)
+    end
+  end
+  
+  describe "associations" do
+    it { should belong_to(:user) }
+    it { should belong_to(:photo) }
+  end
 end
