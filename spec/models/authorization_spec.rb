5a37189d1dc4847a1d79c2ba948565757a5ee53f
Dale Um <dale.um@gmail.com>
2014-01-06 00:47:08 -0500

    creates user_vote specs

--- spec/models/authorization_spec.rb
+++ spec/models/authorization_spec.rb
@@ -1,5 +1,23 @@
 require 'spec_helper'
 
 describe Authorization do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "without provider, uid or user_id" do
+    let(:incomplete_authorization) { authorization = Authorization.new }
+    
+    it "validates presence of provider" do
+      expect(incomplete_authorization).to have(1).error_on(:provider)
+    end 
+    
+    it "validates presence of uid" do
+      expect(incomplete_authorization).to have(1).error_on(:uid)
+    end
+    
+    it "validates presence of user_id" do
+      expect(incomplete_authorization).to have(1).error_on(:user_id)
+    end  
+  end
+  
+  describe "associations" do
+    it { should belong_to(:user) }
+  end
 end
--- spec/models/user_vote_spec.rb
+++ spec/models/user_vote_spec.rb
@@ -1,5 +1,25 @@
 require 'spec_helper'
 
 describe UserVote do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "without value, user_id or photo_id" do
+    let(:incomplete_user_vote) { user_vote = UserVote.new }
+    
+    it "validates presence of value" do
+      expect(incomplete_user_vote).to have(1).error_on(:value)
+    end
+    
+    it "validates presence of user_id" do
+      expect(incomplete_user_vote).to have(1).error_on(:user_id)
+    end
+    
+    it "validates presence of photo_id" do
+      expect(incomplete_user_vote).to have(1).error_on(:photo_id)
+    end
+  
+  end
+  
+  describe "associations" do
+    it { should belong_to(:user) }
+    it { should belong_to(:photo) }
+  end
 end
