04bda7fb06843b9a450857a05b9c30bb09ccb23c
Dale Um <dale.um@gmail.com>
2014-01-06 00:40:58 -0500

    creates region specs

--- spec/models/region_spec.rb
+++ spec/models/region_spec.rb
@@ -1,5 +1,36 @@
 require 'spec_helper'
 
 describe Region do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "without name, user_id or bounds" do
+    let(:incomplete_region) { region = Region.new }
+    
+    it "validates presence of name" do
+      expect(incomplete_region).to have(1).error_on(:name)
+    end
+    
+    it "validates presence of user_id" do
+      expect(incomplete_region).to have(1).error_on(:user_id)
+    end
+    
+    it "validates presence of north_bound" do
+      expect(incomplete_region).to have(1).error_on(:north_bound)
+    end
+    
+    it "validates presence of south_bound" do
+      expect(incomplete_region).to have(1).error_on(:south_bound)
+    end
+    
+    it "validates presence of east_bound" do
+      expect(incomplete_region).to have(1).error_on(:east_bound)
+    end
+    
+    it "validates presence of west_bound" do
+      expect(incomplete_region).to have(1).error_on(:west_bound)
+    end
+  end
+  
+  describe "associations" do
+    it { should belong_to(:user) }
+  end
+  
 end
